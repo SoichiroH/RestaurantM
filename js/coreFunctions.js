20d92417d13d432a080b4fba882580aafb85c8b8
SoichiroH <soichiro.h21@gmail.com>
2016-06-21 14:29:57 -0700

    Initial commit

---
+++ js/coreFunctions.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Soichiro on 6/11/2016.
+ */
+
+$(window).load(function(){
+/*    if ($(window).height() > $(window).width()) {
+        var ratio = $('.parallax').width() / $('.parallax').height();
+        $('.parallax img').css('height', ($(window).height()) + 'px');
+        $('.parallax img').css('width', $('.parallax').height() * ratio + 'px');
+    }*/
+
+//Header
+    //Header initial height
+    $('header').height($(window).height() + 80);
+
+    // Window resize height
+    $(window).resize(function() {
+        $('header').height($(window).height());
+    });
+
+    var map;
+    function initMap() {
+        var myLatLng = {lat: 37.8639302, lng: -122.2686306};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            zoom: 15,
+            scrollwheel: false
+        });
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+        });
+    }
+    initMap();
+
+
+});
+
+
+
