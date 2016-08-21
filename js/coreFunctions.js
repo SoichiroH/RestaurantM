4e21728a1fd6566b69a3b04fcf131d716094839f
SoichiroH <soichiro.h21@gmail.com>
2016-08-20 23:04:47 -0700

    Added Backstretch.js All Sections: Added comingdown on scroll function to all panes and boxes. Header: Added more images and backstretch function. Wappa: Added more images and backstretch function.

--- js/coreFunctions.js
+++ js/coreFunctions.js
@@ -18,13 +18,90 @@ $(window).load(function(){
         $('header').height($(window).height()* 0.84);
     });
 
-    //Scroll Down
-    $('#scrollDownIcon').on('click', function(){
-        var windowHeight = $(window).height();
-        $('body').not(':animated').animate({scrollTop: windowHeight}, 'slow');
+    $('#headerTitle').delay(1000).addClass('comingDown2s');
+
+    $('header').backstretch([
+        "img/header.jpg",
+        "img/sashimi.jpg",
+        "img/wappa4.jpg",
+        "img/robata.jpg",
+        "img/udon.jpg"
+    ], {
+        fade: 1000,
+        duration: 3000
     });
 
 
+//Menu Section
+
+/*
+    $('#menuBoxContainer').addClass('hidden');
+*/
+
+    $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            windowScrollTop = $(this).scrollTop(),
+            menuBoxTop = $('#menuBoxContainer').offset().top,
+            menuBoxHeight = $('#menuBoxContainer').outerHeight(),
+            menuEventPoint = menuBoxTop + menuBoxHeight - windowHeight,
+            weeklyEventPoint = menuEventPoint + menuBoxHeight,
+            weeklyHeight = $('#weekly').outerHeight(),
+            wappaEventPoint = weeklyEventPoint + weeklyHeight,
+            wappaHeight = $('#wappaSection').outerHeight(),
+            happyEventPoint = wappaEventPoint + wappaHeight;
+
+
+        var menuPanes = $('#menuBoxContainer').find('.menu-pane'),
+            weeklyPanes = $('#weekly').find('.weekly-pane'),
+            wappaPanes = $('#wappaSection').find('.wappa-pane'),
+            happyPanes = $('#happyHour').find('.happy-pane');
+
+
+        if (windowScrollTop > menuEventPoint){
+            var x=1;
+            menuPanes.each(function(){
+                $(this).addClass('comingDown'+x+'s');
+                x += 1;
+            });
+        }
+        if (windowScrollTop > weeklyEventPoint){
+            var x=1;
+            weeklyPanes.each(function(){
+                $(this).addClass('comingDown'+x+'s');
+                x += 1;
+            });
+        }
+        if (windowScrollTop > wappaEventPoint){
+            var x=1;
+            wappaPanes.each(function(){
+                $(this).addClass('comingDown'+x+'s');
+                x += 1;
+            });
+        }
+        if (windowScrollTop > happyEventPoint){
+            var x=1;
+            happyPanes.each(function(){
+                $(this).addClass('comingDown'+x+'s');
+                x += 1;
+            });
+        }
+    });
+
+
+//Wappa
+    $('#wappaImages').backstretch([
+        "img/wappa2.jpg",
+        "img/wappa3.jpg",
+        "img/wappa4.jpg",
+        "img/wappa5.jpg",
+        "img/wappa6.jpg",
+    ], {
+        fade: 1000,
+        duration: 2000
+    });
+
+
+//Map
     var map;
     function initMap() {
         var myLatLng = {lat: 37.8639302, lng: -122.2686306};
@@ -36,7 +113,7 @@ $(window).load(function(){
         });
         var marker = new google.maps.Marker({
             position: myLatLng,
-            map: map,
+            map: map
         });
     }
     initMap();
